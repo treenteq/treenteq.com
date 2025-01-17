64515137e09882cadabf6f0a6bc48f940756ee2a
rahulbarmann <itsrahulbarman1@gmail.com>
2025-01-17 20:02:56 +0530

    feat: setup privy and add providers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+        PRIVY_APP_ID: process.env.PRIVY_APP_ID,
+    },
+    /* config options here */
 };
 
 export default nextConfig;
---
+++ src/app/providers.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    const APP_ID = process.env.PRIVY_APP_ID;
+
+    if (!APP_ID) {
+        throw new Error("PRIVY_APP_ID not configured in env");
+    }
+
+    return (
+        <PrivyProvider
+            appId={APP_ID}
+            config={{
+                appearance: {
+                    theme: "dark",
+                    accentColor: "#676FFF",
+                    logo: "https://your-logo-url",
+                },
+                embeddedWallets: {
+                    createOnLogin: "users-without-wallets",
+                },
+            }}
+        >
+            {children}
+        </PrivyProvider>
+    );
+}
