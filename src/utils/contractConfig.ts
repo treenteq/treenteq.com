3d57652dc085949fa5207ebfd4b019b954b6d49e
rahulbarmann <itsrahulbarman1@gmail.com>
2025-01-20 21:33:18 +0530

    chore: add smart contract config

---
+++ src/utils/contractConfig.ts
@@ -0,0 +1,4 @@
+export const CONTRACT_ADDRESS =
+    "0x14A534D22F921AcC707CC061f175CD0C66A22f05" as const;
+export const RPC_URL =
+    "https://base-sepolia.g.alchemy.com/v2/d73xweGEFs_xm9nqS9ofwPYYr_xrO9JJ";
