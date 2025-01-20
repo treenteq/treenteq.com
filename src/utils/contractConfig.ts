6b42c2c7ac5ef2fd2e7b3776d755ae7b6837aa34
rahulbarmann <itsrahulbarman1@gmail.com>
2025-01-21 00:57:46 +0530

    chore: update contract address and favicon

--- src/utils/contractConfig.ts
+++ src/utils/contractConfig.ts
@@ -1,4 +1,4 @@
 export const CONTRACT_ADDRESS =
-    "0x14A534D22F921AcC707CC061f175CD0C66A22f05" as const;
+    "0x21e4e971f4b0233eC850eD419DDC2eb6182f6E4e" as const;
 export const RPC_URL =
     "https://base-sepolia.g.alchemy.com/v2/d73xweGEFs_xm9nqS9ofwPYYr_xrO9JJ";
