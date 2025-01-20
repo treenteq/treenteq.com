31a211ee881678923d91b62c079ef5ec2d191e16
rahulbarmann <itsrahulbarman1@gmail.com>
2025-01-20 21:30:54 +0530

    chore: update env vars

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     env: {
         PRIVY_APP_ID: process.env.PRIVY_APP_ID,
+        PINATA_JWT: process.env.PINATA_JWT,
+        PINATA_API_KEY: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+        PINATA_SECRET_KEY: process.env.NEXT_PUBLIC_PINATA_SECRET_KEY,
+        NEXT_PUBLIC_CONTRACT_OWNER_PRIVATE_KEY:
+            process.env.NEXT_PUBLIC_CONTRACT_OWNER_PRIVATE_KEY,
     },
     /* config options here */
 };
