af24f9f785badb7e3374d00c4e18ab6a5033caf3
rahulbarmann <itsrahulbarman1@gmail.com>
2025-01-26 21:09:10 +0530

    chore: update contract address and abi for fractional ownership implementation

--- src/utils/contractConfig.ts
+++ src/utils/contractConfig.ts
@@ -1,4 +1,4 @@
 export const CONTRACT_ADDRESS =
-    "0x21e4e971f4b0233eC850eD419DDC2eb6182f6E4e" as const;
+    "0x1D56b015A11F34a3EFB298f30953D74B871F3f64" as const;
 export const RPC_URL =
     "https://base-sepolia.g.alchemy.com/v2/d73xweGEFs_xm9nqS9ofwPYYr_xrO9JJ";
